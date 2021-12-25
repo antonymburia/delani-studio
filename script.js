b2452353c764c4dbd536ce18e21eeb5975717620
antonymburia <antonymburia10@gmail.com>
2021-12-25 15:41:39 +0300

    create a function to collect form data and show popup

--- script.js
+++ script.js
@@ -87,3 +87,8 @@ $(document).ready(function() {
     $("#pr-8").hide();
   });
 });
+// send message
+function sendmessage() {
+  var sender_name = document.getElementById('name').value;
+  alert('Thank you ' + sender_name + ' we have received your message. Thank you for reaching out to us');
+}
