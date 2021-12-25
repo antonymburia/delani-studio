c5a24455f4b66c063b0e6048450f99533be902e9
antonymburia <antonymburia10@gmail.com>
2021-12-25 15:26:27 +0300

    create jquery funtions to show a white box on hover and modify css to show the output

--- script.js
+++ script.js
@@ -27,4 +27,63 @@ $(document).ready(function() {
     $("#manage").hide();
     $(".img3").show();
   });
+  // hide/show project names
+  $(".txt-front").hide();
+  // hover effect
+  // project 1
+  $(".pr-1").hover(function(){
+    $("#pr-1").show();
+  });
+  $(".pr-1").mouseleave(function(){
+    $("#pr-1").hide();
+  });
+  // project 2
+  $(".pr-2").hover(function(){
+    $("#pr-2").show();
+  });
+  $(".pr-2").mouseleave(function(){
+    $("#pr-2").hide();
+  });
+  // project 3
+  $(".pr-3").hover(function(){
+    $("#pr-3").show();
+  });
+  $(".pr-3").mouseleave(function(){
+    $("#pr-3").hide();
+  });
+  // project 4
+  $(".pr-4").hover(function(){
+    $("#pr-4").show();
+  });
+  $(".pr-4").mouseleave(function(){
+    $("#pr-4").hide();
+  });
+  // project 5
+  $(".pr-5").hover(function(){
+    $("#pr-5").show();
+  });
+  $(".pr-5").mouseleave(function(){
+    $("#pr-5").hide();
+  });
+  // project 6
+  $(".pr-6").hover(function(){
+    $("#pr-6").show();
+  });
+  $(".pr-6").mouseleave(function(){
+    $("#pr-6").hide();
+  });
+  // project 7
+  $(".pr-7").hover(function(){
+    $("#pr-7").show();
+  });
+  $(".pr-7").mouseleave(function(){
+    $("#pr-7").hide();
+  });
+  // project 8
+  $(".pr-8").hover(function(){
+    $("#pr-8").show();
+  });
+  $(".pr-8").mouseleave(function(){
+    $("#pr-8").hide();
+  });
 });
